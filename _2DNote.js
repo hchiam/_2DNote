f669a85105f810c54e0ad5d509535b172d0e7ddb
hchiam <hchiam@users.noreply.github.com>
2020-09-09 18:02:15 -0400

    auto-format

--- _2DNote.js
+++ _2DNote.js
@@ -1,7 +1,6 @@
 // example usage: _2DNote.play(...) or _2DNote.update(...)
 
-var _2DNote = (function() {
-
+var _2DNote = (function () {
   var audioContext = new AudioContext();
   var note = null;
 
@@ -11,20 +10,22 @@ var _2DNote = (function() {
   var comfyFrequencyRange = [150, 400];
   var comfyVolumeRange = [0, 0.5]; // technically gain (ranges from 0 to 1)
 
-  function setAs2DArea(e, callbackUponUpdate) { // e = event or element
+  function setAs2DArea(e, callbackUponUpdate) {
+    // e = event or element
     // example usage: _2DNote.setAs2DArea(document.getElementById('2d-area', callbackUponUpdate));
     this.callbackUponUpdate = callbackUponUpdate;
     const element = e ? e : document.body;
-    element.addEventListener('mousedown', this.play.bind(this));
-    element.addEventListener('mouseup', this.stop.bind(this));
-    element.addEventListener('mousemove', this.update.bind(this));
-    element.addEventListener('touchstart', this.play.bind(this));
-    element.addEventListener('touchend', this.stop.bind(this));
-    element.addEventListener('touchmove', this.update.bind(this));
+    element.addEventListener("mousedown", this.play.bind(this));
+    element.addEventListener("mouseup", this.stop.bind(this));
+    element.addEventListener("mousemove", this.update.bind(this));
+    element.addEventListener("touchstart", this.play.bind(this));
+    element.addEventListener("touchend", this.stop.bind(this));
+    element.addEventListener("touchmove", this.update.bind(this));
     this.setupExitedViewDetection(element);
   }
 
-  function play(e) { // e = event or element
+  function play(e) {
+    // e = event or element
     // example usage: <body onmousedown="_2DNote.play(event);" style="width: 100vw; height: 100vh;" ontouchstart="_2DNote.play(event);"></body>
     this.stop();
     this.setupExitedViewDetection(e);
@@ -34,7 +35,7 @@ var _2DNote = (function() {
     volumeSetup.connect(this.audioContext.destination);
     volumeSetup.gain.value = isNaN(volume) ? 0.5 : volume;
     var oscillator = this.audioContext.createOscillator();
-    oscillator.type = 'sine';
+    oscillator.type = "sine";
     oscillator.frequency.value = isNaN(frequency) ? 400 : frequency;
     oscillator.connect(volumeSetup);
     // instead of oscillator.connect(this.audioContext.destination);
@@ -48,7 +49,8 @@ var _2DNote = (function() {
   }
 
   var callbackUponUpdate = null;
-  function update(e, callback) { // e = event or element
+  function update(e, callback) {
+    // e = event or element
     if (!this.note) return;
     var frequency = this.getFrequency(e);
     var volume = this.getVolume(e);
@@ -76,10 +78,10 @@ var _2DNote = (function() {
     const element = e ? e : document.body;
     console.log(element);
     if (element.removeEventListener && element.addEventListener) {
-      element.removeEventListener('mouseleave', this.warnExitedView);
-      element.removeEventListener('touchcancel', this.warnExitedView);
-      element.addEventListener('mouseleave', this.warnExitedView);
-      element.addEventListener('touchcancel', this.warnExitedView);
+      element.removeEventListener("mouseleave", this.warnExitedView);
+      element.removeEventListener("touchcancel", this.warnExitedView);
+      element.addEventListener("mouseleave", this.warnExitedView);
+      element.addEventListener("touchcancel", this.warnExitedView);
     }
   }
 
@@ -87,68 +89,84 @@ var _2DNote = (function() {
     const element = e ? e : document.body;
     var screenWidth = element.clientWidth;
     var screenHeight = element.clientHeight;
-    var simulatedCenterClick = { // center: guaranteed != edge
+    var simulatedCenterClick = {
+      // center: guaranteed != edge
       currentTarget: true,
       clientX: screenWidth / 2,
       clientY: screenHeight / 2,
     };
     _2DNote.play(simulatedCenterClick);
-    setTimeout(function() {
+    setTimeout(function () {
       _2DNote.stop();
       if (element.removeEventListener) {
-        element.removeEventListener('mouseleave', _2DNote.warnExitedView);
-        element.removeEventListener('touchcancel', _2DNote.warnExitedView);  
+        element.removeEventListener("mouseleave", _2DNote.warnExitedView);
+        element.removeEventListener("touchcancel", _2DNote.warnExitedView);
       }
     }, 100);
   }
 
-  function getFrequency(e) { // e = event or element
+  function getFrequency(e) {
+    // e = event or element
     var x = this.getX(e);
-    var frequency = this.normalize(x, this.viewXRange, this.comfyFrequencyRange);
+    var frequency = this.normalize(
+      x,
+      this.viewXRange,
+      this.comfyFrequencyRange
+    );
     return frequency;
   }
 
-  function getVolume(e) { // e = event or element
+  function getVolume(e) {
+    // e = event or element
     var y = this.getY(e);
     // technically getting gain (which ranges 0 to 1)
     var volume = this.normalize(y, this.viewYRange, this.comfyVolumeRange);
     return volume;
   }
 
-  function getX(e) { // e = event or element
-    var isMouseEvent = (e.currentTarget && e.clientX);
-    var isTouchEvent = (e.touches);
+  function getX(e) {
+    // e = event or element
+    var isMouseEvent = e.currentTarget && e.clientX;
+    var isTouchEvent = e.touches;
     if (isMouseEvent) {
       return e.clientX;
     } else if (isTouchEvent) {
       return e.touches[0].clientX;
-    } else { // element
+    } else {
+      // element
       return e.offsetLeft;
     }
   }
 
-  function getY(e) { // e = event or element
-    var isMouseEvent = (e.currentTarget && e.clientY);
-    var isTouchEvent = (e.touches);
+  function getY(e) {
+    // e = event or element
+    var isMouseEvent = e.currentTarget && e.clientY;
+    var isTouchEvent = e.touches;
     if (isMouseEvent) {
       return e.clientY;
     } else if (isTouchEvent) {
       return e.touches[0].clientY;
-    } else { // element
+    } else {
+      // element
       return e.offsetTop;
     }
   }
 
-  function normalize(value, [inputRangeMin,inputRangeMax], [outputRangeMin,outputRangeMax]) {
+  function normalize(
+    value,
+    [inputRangeMin, inputRangeMax],
+    [outputRangeMin, outputRangeMax]
+  ) {
     var inputBias = value - inputRangeMin;
-    var ratioAdjustment = (outputRangeMax - outputRangeMin) / (inputRangeMax - inputRangeMin);
+    var ratioAdjustment =
+      (outputRangeMax - outputRangeMin) / (inputRangeMax - inputRangeMin);
     var outputBias = outputRangeMin;
     return inputBias * ratioAdjustment + outputBias;
   }
 
   function copy() {
     /**
-     * If you want to play multiple notes at the same time, 
+     * If you want to play multiple notes at the same time,
      * you can use this to create more instances.
      */
     return recursiveDeepCopy(this);
@@ -157,7 +175,7 @@ var _2DNote = (function() {
         return object;
       } else if (Array.isArray(object)) {
         var arrayCopy = [];
-        object.forEach(function(element) {
+        object.forEach(function (element) {
           arrayCopy.push(recursiveDeepCopy(element));
         });
         return arrayCopy;
@@ -165,7 +183,7 @@ var _2DNote = (function() {
         var objectCopy = {};
         for (var property in object) {
           if (object.hasOwnProperty(property)) {
-            if (property === 'audioContext') {
+            if (property === "audioContext") {
               objectCopy.audioContext = new AudioContext();
             } else {
               objectCopy[property] = recursiveDeepCopy(object[property]);
